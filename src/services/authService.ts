0421d87b88d428ba00abc5fffd16a1aafcd2c8cd
Adrian-Codifica <adrianfr2011@gmail.com>
2023-04-29 20:49:27 +0200

    start login

---
+++ src/services/authService.ts
@@ -0,0 +1,43 @@
+import axios from '../utils/config/axios.config';
+
+/**
+ * Login Method
+ * @param {string} email Email to login a user
+ * @param {string} password Password to login a user
+ * @returns 
+ */
+export const login = (email: string, password: string) => {
+
+    // Declare Body to POST
+    let body = {
+        email: email,
+        password: password
+    }
+    
+    // send POST request to login endpoint
+    // http://localhost:8000/api/auth/login
+    return axios.post('/auth/login', body);
+}
+
+/**
+ * Register Method
+ * @param {string} name Name of user
+ * @param {string} email Email of user
+ * @param {string} password Password of user
+ * @param {number} age Age of user
+ * @returns 
+ */
+export const register = (name: string, email: string, password: string, age: number) => {
+
+    // Declare Body to POST
+    let body = {
+        name: name,
+        email: email,
+        password: password,
+        age: age
+    }
+
+    // Send POST request to register end point
+    // http://localhost:8000/api/auth/register
+    return axios.post('/auth/register', body);
+}
