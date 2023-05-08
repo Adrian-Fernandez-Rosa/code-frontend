815677b42832dfc444d33dd60c80ce277b59d5a3
Adrian-Codifica <adrianfr2011@gmail.com>
2023-05-08 02:17:11 +0200

    second editor completed

--- src/components/editor/NewEditor.tsx
+++ src/components/editor/NewEditor.tsx
@@ -23,55 +23,46 @@ async function getAverage() {
 
 // Define Styles for Editor
 const styles: any = {
-    root: {
-        boxSizing: 'border-box',
-        fontFamily: '"Dank Mono", "Fira Code", "monospace"',
-        ...myTheme.plain //estilos principales
+  root: {
+    boxSizing: 'border-box',
+    fontFamily: '"Dank Mono", "Fira Code", monospace',
+    ...themes.nightOwl.plain //estilos principales
 
-    }
+  }
 }
 
 // Highlight Component
-const HighlightElement = (code: string) => (
-
-    <Highlight
-    theme={themes.shadesOfPurple}
-    code={codeSnippet}
-    language="tsx"
-  >
-    {({ className, style, tokens, getLineProps, getTokenProps }) => (
-     <Fragment>
-                {tokens.map((line, i) => (
-                    <div {...getLineProps({ line: line, key: i })}>
-                        {
-                            line.map((token, key) =>
-                                <span {...getTokenProps({ token, key })} />
-
-                            )
-                        }
-                    </div>
-                ))}
-     </Fragment>
-    )}
+const HighlightElement2 = (code: string) => (
+  <Highlight  theme={myTheme} code={code} language='tsx'>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <Fragment>
+              {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                      {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
+                  </div>
+              ))}
+          </Fragment>
+      )}
   </Highlight>
 );
 
+
 export const NewEditor = () => {
 
-    const [code, setCode] = useState(codeSnippet);
+  const [code, setCode] = useState(codeSnippet);
 
-    const handleChange = (newCode: string) => {
-        setCode(newCode)
-    }
+  const handleChange = (newCode: string) => {
+    setCode(newCode)
+  }
 
-    return (
-        <Editor
-            value={code}
-            onValueChange={handleChange}
-            highlight={HighlightElement}
-            padding={10}
-            style={styles.root}
-        />
-    )
+  return (
+    <Editor
+      value={code}
+      onValueChange={handleChange}
+      highlight={HighlightElement2}
+      padding={10}
+      style={styles.root}
+    />
+  )
 
 }
